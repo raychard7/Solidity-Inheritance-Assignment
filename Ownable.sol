96e5a9ddc38cf1ac432c38b033e9348fb1bb7c64
Remix Workspace <>
2022-10-08 12:50:55 -0700

    First Commit

---
+++ Ownable.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.5;
+
+contract Ownable{
+
+    address public owner ;
+
+modifier onlyOwner{
+
+        require(msg.sender == owner);
+
+        _; // run function
+
+}
+
+   constructor(){
+
+        owner= msg.sender;
+
+    }
+
+// a tiny function placed to run before the main function code.
+// If you have a piece of code that is going to be used alot for different fxns.
+// require(msg.sender == owner), is called with modifier onlyOwner
+//add onlyOwner to fxn after visibility.
+   
+}
